61460afbc15d49667041396aafe9818959bba5e0
nasty091 <6151071035@st.utc2.edu.vn>
2022-10-23 13:18:33 +0700

    giao dien

---
+++ BTL_ClockManage/Customer_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_ClockManage
+{
+    public partial class Customer_Manager : UserControl
+    {
+        public Customer_Manager()
+        {
+            InitializeComponent();
+        }
+    }
+}
